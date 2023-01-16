0db0a585a959f1c45993c608a75a14b9261742c0
Louis Dole <louis.dole@live.fr>
2023-01-16 21:31:01 +0100

    Création du fichier HTML et mise en place du CSS

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class HomeController extends AbstractController
+{
+    // Annotation qui permet à Symfony de retrouver quelle route correspond à quelle fonction 
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig', [
+        ]);
+    }
+}
